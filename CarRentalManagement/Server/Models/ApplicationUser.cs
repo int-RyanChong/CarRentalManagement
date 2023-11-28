ac9bcf6c67a1386ced76031577ad37da9aecbfb0
int-RyanChong <2102197G@student.tp.edu.sg>
2023-11-28 14:42:29 +0800

    Revert "22_11_2023"
    This reverts commit 776c3d47fe3f0ef958f94bd74e7f1611d12d776a.

--- CarRentalManagement/Server/Models/ApplicationUser.cs
+++ CarRentalManagement/Server/Models/ApplicationUser.cs
@@ -4,8 +4,5 @@ namespace CarRentalManagement.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set;}
-
     }
 }
