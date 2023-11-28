3bdcc8fee145770578b78c2a4376bf86cd358245
int-RyanChong <2102197G@student.tp.edu.sg>
2023-11-28 17:13:23 +0800

    COMPLETE All Controllers Refactor

---
+++ CarRentalManagement/Server/Controllers/BookingsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        // Refactored
+        private readonly IUnitOfWork _unitOfWork;
+
+        // Refactored
+        public BookingsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Bookings
+        [HttpGet]
+        public async Task<IActionResult> GetBookings()
+        {
+            // Refactored
+            var Bookings = await _unitOfWork.Bookings.GetAll();
+            return Ok(Bookings);
+        }
+
+        // GET: api/Bookings/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBooking(int id)
+        {
+            //Refactored
+            var Booking = await _unitOfWork.Bookings.Get(q => q.Id == id);
+
+            if (Booking == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Booking);
+        }
+
+        // PUT: api/Bookings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBooking(int id, Booking Booking)
+        {
+            if (id != Booking.Id)
+            {
+                return BadRequest();
+            }
+
+            //_context.Entry(Booking).State = EntityState.Modified;
+            // Refactored
+            _unitOfWork.Bookings.Update(Booking);
+
+            try
+            {
+                //await _context.SaveChangesAsync();
+                // Refactored
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                // if (!BookingExists(id))
+                // Refactored
+                if (!await BookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bookings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Booking>> PostBooking(Booking Booking)
+        {
+
+            //if (_context.Bookings == null)
+            //{
+            //    return Problem("Entity set 'ApplicationDbContext.Bookings'  is null.");
+            //}
+            //  _context.Bookings.Add(Booking);
+            //  await _context.SaveChangesAsync();
+
+            await _unitOfWork.Bookings.Insert(Booking);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetBooking", new { id = Booking.Id }, Booking);
+        }
+
+        // DELETE: api/Bookings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBooking(int id)
+        {
+            //if (_context.Bookings == null)
+            //{
+            //    return NotFound();
+            //}
+            //var Booking = await _context.Bookings.FindAsync(id);
+            // Refactor
+            var Booking = await _unitOfWork.Bookings.Get(q => q.Id == id);
+            if (Booking == null)
+            {
+                return NotFound();
+            }
+
+            //_context.Bookings.Remove(Booking);
+            //await _context.SaveChangesAsync();
+
+            await _unitOfWork.Bookings.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+
+        //private bool BookingExists(int id)
+        // Refactored
+
+        private async Task<bool> BookingExists(int id)
+        {
+            //return (_context.Bookings?.Any(e => e.Id == id)).GetValueOrDefault();
+            // Refactor
+            var Booking = await _unitOfWork.Bookings.Get(q => q.Id == id);
+            return Booking != null;
+        }
+    }
+}
---
+++ CarRentalManagement/Server/Controllers/CustomersController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // Refactored
+        private readonly IUnitOfWork _unitOfWork;
+
+        // Refactored
+        public CustomersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers()
+        {
+            // Refactored
+            var Customers = await _unitOfWork.Customers.GetAll();
+            return Ok(Customers);
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomer(int id)
+        {
+            //Refactored
+            var Customer = await _unitOfWork.Customers.Get(q => q.Id == id);
+
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Customer);
+        }
+
+        // PUT: api/Customers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(int id, Customer Customer)
+        {
+            if (id != Customer.Id)
+            {
+                return BadRequest();
+            }
+
+            //_context.Entry(Customer).State = EntityState.Modified;
+            // Refactored
+            _unitOfWork.Customers.Update(Customer);
+
+            try
+            {
+                //await _context.SaveChangesAsync();
+                // Refactored
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                // if (!CustomerExists(id))
+                // Refactored
+                if (!await CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer Customer)
+        {
+
+            //if (_context.Customers == null)
+            //{
+            //    return Problem("Entity set 'ApplicationDbContext.Customers'  is null.");
+            //}
+            //  _context.Customers.Add(Customer);
+            //  await _context.SaveChangesAsync();
+
+            await _unitOfWork.Customers.Insert(Customer);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetCustomer", new { id = Customer.Id }, Customer);
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            //if (_context.Customers == null)
+            //{
+            //    return NotFound();
+            //}
+            //var Customer = await _context.Customers.FindAsync(id);
+            // Refactor
+            var Customer = await _unitOfWork.Customers.Get(q => q.Id == id);
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+
+            //_context.Customers.Remove(Customer);
+            //await _context.SaveChangesAsync();
+
+            await _unitOfWork.Customers.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+
+        //private bool CustomerExists(int id)
+        // Refactored
+
+        private async Task<bool> CustomerExists(int id)
+        {
+            //return (_context.Customers?.Any(e => e.Id == id)).GetValueOrDefault();
+            // Refactor
+            var Customer = await _unitOfWork.Customers.Get(q => q.Id == id);
+            return Customer != null;
+        }
+    }
+}
