706f2d4237561d16f5cdd859c7f93322a76ed6df
int-RyanChong <2102197G@student.tp.edu.sg>
2024-01-06 19:00:08 +0800

    Database Bug Fix

---
+++ CarRentalManagement/Server/Migrations/20240106105602_dbvalidation.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentalManagement.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbvalidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LicensePlateNumber",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Models",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Makes",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Customers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Customers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailAddress",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DrivingLicense",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactNumber",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Colours",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3781efa7-66dc-47f0-860f-e506d04102e4",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "9ac5f7e2-d57e-4690-82b9-d38cc9fdb709", "AQAAAAIAAYagAAAAEE6eCqaoNJfaRiYvi2s5wPnjyqYP+k3AdameFGf9welXYtOTpSZ8huDGBIhNyLIOiw==", "33389641-e427-4112-bf22-6fdfcc5eef92" });
+
+            migrationBuilder.UpdateData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(455), new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(456) });
+
+            migrationBuilder.UpdateData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(458), new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(458) });
+
+            migrationBuilder.UpdateData(
+                table: "Makes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(79), new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(89) });
+
+            migrationBuilder.UpdateData(
+                table: "Makes",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(91), new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(92) });
+
+            migrationBuilder.UpdateData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(667), new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(668) });
+
+            migrationBuilder.UpdateData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(670), new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(671) });
+
+            migrationBuilder.UpdateData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(672), new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(672) });
+
+            migrationBuilder.UpdateData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(674), new DateTime(2024, 1, 6, 18, 56, 2, 544, DateTimeKind.Local).AddTicks(674) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LicensePlateNumber",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Models",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Makes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailAddress",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DrivingLicense",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactNumber",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Colours",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3781efa7-66dc-47f0-860f-e506d04102e4",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "5aebc7f7-6528-4bcd-82ec-35e5abb698be", "AQAAAAIAAYagAAAAEBZa37Kx4w78JXh/NUuw5a/7KF4cE7rhnlE/kQt5p5v7Kt+gaZCE5pc6xXhHUwEkqA==", "c6251fb6-39d7-440f-a8bf-f90519248ae9" });
+
+            migrationBuilder.UpdateData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6435), new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6436) });
+
+            migrationBuilder.UpdateData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6438), new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6439) });
+
+            migrationBuilder.UpdateData(
+                table: "Makes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6054), new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6063) });
+
+            migrationBuilder.UpdateData(
+                table: "Makes",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6065), new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6066) });
+
+            migrationBuilder.UpdateData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6665), new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6666) });
+
+            migrationBuilder.UpdateData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6668), new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6669) });
+
+            migrationBuilder.UpdateData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6670), new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6670) });
+
+            migrationBuilder.UpdateData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6671), new DateTime(2024, 1, 3, 11, 40, 11, 358, DateTimeKind.Local).AddTicks(6672) });
+        }
+    }
+}
